337a53755c46df9adf5baa0b8bae4726868f849a
ahmed atta <ahmedatta3322@gmail.com>
2018-08-17 13:44:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
 // add solution here
 function theBeatlesPlay(singers,instr){
   var n = []
-  for(var i = 0 ; i ; n.length){
-    n.push(singers[i]+instr[i])
+  for(var i = 0 ; i < singers.length ; i++ ){
+    n.push(singers[i] + " plays " +instr[i])
   }
   return n
+}
+function johnLennonFacts(facts){
+  var n = []
+  while(facts.length > 0){
+    n.push(facts.shift() + "!!!")
+  }
+  return n 
 }
